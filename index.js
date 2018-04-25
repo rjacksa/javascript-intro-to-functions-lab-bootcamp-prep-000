6417f716a9c0644f6163d1f7cf47aa2dee85e742
rebeccajachtenberg@gmail.com <rebeccajachtenberg@gmail.com>
2018-04-25 19:16:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase // true
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase // true
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase // false
+mixedCase.toUpperCase() === mixedCase //false
+function sayHiToGrandma(string) {
+  if (mixedCase) { 
+   return "I love you, too."
+  } else if (upperCase) { 
+    return "YES INDEED!"
+  } else if (lowerCase) {
+    return "I can't hear you!"
+  }
+}
